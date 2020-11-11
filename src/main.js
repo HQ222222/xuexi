24c788b8fd7e3c718bb1f57257b3f01e523062e9
hq <1874535456@qq.com>
2020-11-11 17:27:01 +0800

    用户的删除和修改，增加

--- src/main.js
+++ src/main.js
@@ -9,6 +9,14 @@ import './assets/font/iconfont.css';
 import './assets/css/global.css';
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+axios.interceptors.request.use((config) => {
+  // eslint-disable-next-line no-console
+  console.log(config);
+  // eslint-disable-next-line no-param-reassign
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+});
+
 Vue.prototype.$http = axios;
 
 Vue.use(ElementUI);
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Login from '../components/Login.vue';
 import Home from '../components/home.vue';
+import Welcome from '../components/Welcome.vue';
+import users from '../components/user/users.vue';
 
 Vue.use(VueRouter);
 
@@ -26,7 +28,13 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home },
+    {
+      path: '/home',
+      component: Home,
+      children: [{ path: '/Welcome', component: Welcome },
+        { path: '/users', component: users }],
+    },
+
   ],
 });
 
